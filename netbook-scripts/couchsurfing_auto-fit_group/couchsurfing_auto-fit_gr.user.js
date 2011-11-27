eb61eadd2b2a4b89b106b69699d44429c61599f1
Jonathan Ballet <jon@multani.info>
2011-11-27 09:37:58 +0800

    Also fits personnal messages

--- netbook-scripts/couchsurfing_auto-fit_group/couchsurfing_auto-fit_gr.user.js
+++ netbook-scripts/couchsurfing_auto-fit_group/couchsurfing_auto-fit_gr.user.js
@@ -3,6 +3,7 @@
 // @namespace      http://multani.info/projects/greasemonkey
 // @description    Auto-fit in the current width messages from Couchsrufing groups
 // @include        http://www.couchsurfing.org/group_read.html*
+// @include        http://www.couchsurfing.org/messages.html*
 // ==/UserScript==
 
 
